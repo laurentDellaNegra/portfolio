0c3e0c36ffa7d3099244d81cd804e7e7c8828203
laurentDellaNegra <laurent.dellanegra@gmail.com>
2024-10-15 21:28:45 +0200

    💚 fix CI

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,6 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  base: 'portfolio',
   integrations: [tailwind(), mdx(), react()],
   site: 'https://ldellanegra.com',
 });
