2da9d86fa50dae93685ae74eccdb76dd860f2242
laurentDellaNegra <laurent.dellanegra@gmail.com>
2024-11-10 22:16:15 +0100

    🔍 Add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,11 @@ import mdx from '@astrojs/mdx';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), react()],
+  integrations: [tailwind(), mdx(), react(), sitemap()],
   site: 'https://ldellanegra.com',
   trailingSlash: "never",
   build: {
